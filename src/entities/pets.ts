63a5a0eb8d54d728b1b350b562168f5da3330a30
Pantaleón Ferrer <61870005+Zettsuen@users.noreply.github.com>
2022-12-11 12:29:01 +0100

    Pets helpers and re-organize the start of the application
    I have created all the CRUD pets helpers for the application.
    
    Also i have re-created the entry point of the application to wait to start the infrastructure to start all the others services because any service needs to access to the infrastructure since the first time and i let the same globalization of some components but using an async function.

--- src/entities/pets.ts
+++ src/entities/pets.ts
@@ -1,4 +1,35 @@
-interface RequestGetPets {
+import { generalOptions } from "./mongodb";
+
+export interface RequestGetPets {
     key?: string;
     name?: string;
+}
+
+export interface GetPetsHelper extends generalOptions {
+    filters: {
+        key?: string;
+        name?: string;
+    }
+}
+
+export interface CreatePetHelper {
+    name: string;
+    color?: string;
+    age?: string;
+    breed?: string;
+    dogPound: string;
+}
+
+export interface UpdatePetHelper {
+    data: CreatePetHelper,
+    filters: {
+        key?: string;
+        name?: string;
+    }
+}
+
+export interface DeletePetHelper {
+    key?: string;
+    name?: string;
+    _id?: string;
 }
