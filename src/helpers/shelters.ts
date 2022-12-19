f94dfc44c8f52855b655c5fa7ab7aaf0d2b34e04
Xavi Ibáñez Martínez <241xavi@gmail.com>
2022-12-19 11:14:05 +0100

    add shelters crud

---
+++ src/helpers/shelters.ts
@@ -0,0 +1,66 @@
+import { CreateShelterHelper, DeleteShelterHelper, GetSheltersHelper, UpdateShelterHelper } from '../entities/models/shelters';
+import { Databases } from "../infrastructure/databases/databases";
+import { v1 } from "uuid";
+import { DeserializerForMongoOptions } from '../utils/DeserializerForMongoHelper';
+
+
+export class SheltersHelper {
+
+    private databases: Databases;
+    private collectionName: string = "shelters";
+
+    constructor(databases: Databases) {
+        this.databases = databases;
+    }
+
+    async getShelters(options: GetSheltersHelper) {
+        options.filters = await DeserializerForMongoOptions(options.filters);
+
+        const data = await this.databases.getClients().mongo.collection(this.collectionName).find(options.filters, options.options).toArray();
+        const rCount = await this.databases.getClients().mongo.collection(this.collectionName).countDocuments(options.filters);
+
+        return [data, rCount];
+
+    }
+
+    async createShelter(options: CreateShelterHelper) {
+
+        const toAdd = {
+            key: v1(),
+            ...options,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }
+
+        await this.databases.getClients().mongo.collection(this.collectionName).insertOne(toAdd);
+
+        return toAdd;
+
+    }
+
+    async updateShelter(options: UpdateShelterHelper) {
+        options.filters = await DeserializerForMongoOptions(options.filters);
+        await this.databases.getClients().mongo.collection(this.collectionName).updateOne(options.filters, {
+            $set: {
+                ...options.data,
+                updatedAt: new Date(),
+            }
+        });
+
+        return options;
+
+    }
+
+    async deleteShelter(options: DeleteShelterHelper) {
+
+        await this.databases.getClients().mongo.collection(this.collectionName).updateOne(options, {
+            $set: {
+                deletedAt: new Date(),
+            }
+        });
+
+        return options;
+
+    }
+
+}
