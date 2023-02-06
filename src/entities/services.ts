dd21040cf317631b242a7ea7bd676b27bc169887
Xavi Ibáñez Martínez <241xavi@gmail.com>
2023-02-07 00:43:42 +0100

    add useres into indexs

--- src/entities/services.ts
+++ src/entities/services.ts
@@ -2,10 +2,12 @@ import { AuthServices } from "../services/auth";
 import { PetsServices } from "../services/pets";
 import { PetsTypesServices } from "../services/petsTypes";
 import { SheltersServices } from "../services/shelters";
+import { UsersServices } from "../services/users";
 
 export interface services {
     pets: PetsServices,
     petsTypes: PetsTypesServices,
     auth: AuthServices,
     shelters: SheltersServices,
+    users: UsersServices,
 }
