38c4fcc69cc943fada32b38d152fc961482cf258
Xavi Ibáñez Martínez <241xavi@gmail.com>
2023-02-18 01:19:09 +0100

    update pets test

--- src/__tests__/pets.test.ts
+++ src/__tests__/pets.test.ts
@@ -8,11 +8,50 @@ beforeAll(async () => {
   })
 
 describe('pets', () => {
+    describe('Get pets', () => {
+        it('It should return data as array', async () => {
+            const data = await services.pets.getPets({});
+            expect(data).toBeInstanceOf(Array);
+        });
+
+        it('It should return that the returned data is larger than 0', async () => {
+            const data = await services.pets.getPets({});
+            expect(data?.[0]?.length > 0).toBe(true);
+        });
+
+        it('It should return that the returned data is equal to count', async () => {
+            const data = await services.pets.getPets({});
+            expect(data?.[0]?.length == data?.[1]).toBe(true);
+        });
+
+        it('It should return that the returned data is 0 when pet does not exists', async () => {
+            const data = await services.pets.getPets({key: "test"});
+            expect(data?.[0]?.length == 0).toBe(true);
+        });
+    });
+    
     describe('Create pets', () => {
+        //TODO: user is not logged
+        it('User is not logged', async () => {
+            try {
+                await services.pets.createPet({} as any);
+            }
+            catch(e) {
+                expect(e).toEqual({
+                    ok: false,
+                    status: 400,
+                    message: "There are required values that don't have a valid value"
+                });
+            }
+            
+        });
+
+        //TODO user is logged
         it('It should return an error at create because there are no params', async () => {
             try {
                 await services.pets.createPet({} as any);
-            }catch(e) {
+            }
+            catch(e) {
                 expect(e).toEqual({
                     ok: false,
                     status: 400,
@@ -37,20 +76,8 @@ describe('pets', () => {
                     message: "There are required values that don't have a valid value"
                 });
             }
-            
         });
     });
-    describe('Create pets', () => {
-        it('It should return data as array', async () => {
-            const pet = await services.pets.getPets({});
-            expect(pet).toBeInstanceOf(Array)
-            
-        });
-
-        it('It should return that the returned data is larger than 0', async () => {
-            const pet = await services.pets.getPets({});
-            expect(pet?.[0]?.length > 0).toBe(true)
-            
-        });
-    });
+    //TODO: describe update pets
+    //TODO: describe delete pets
 });
